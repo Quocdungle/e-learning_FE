44896ebce7daa90f3de4f1cc38181d22982f26cb
Ngọc Châu <dnchau2310@gmail.com>
2023-01-03 23:20:58 +0700

    implement detail course

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -8,16 +8,16 @@ import {
   Stack,
   Text,
   VStack,
-} from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllCourses } from "../../redux/actions/course";
-import toast from "react-hot-toast";
-import { addToPlaylist } from "../../redux/actions/profile";
-import { loadUser } from "../../redux/actions/user";
-import { Pagination } from "antd";
-import "./courses.scss";
+} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCourses } from '../../redux/actions/course';
+import toast from 'react-hot-toast';
+import { addToPlaylist } from '../../redux/actions/profile';
+import { loadUser } from '../../redux/actions/user';
+import { Pagination } from 'antd';
+import './courses.scss';
 
 const Course = ({
   views,
@@ -31,13 +31,13 @@ const Course = ({
   loading,
 }) => {
   return (
-    <VStack className="course" alignItems={["center", "flex-start"]}>
-      <Image src={imageSrc} boxSize="60" objectFit={"contain"} />
+    <VStack className='course' alignItems={['center', 'flex-start']}>
+      <Image src={imageSrc} boxSize='60' objectFit={'contain'} />
       <Heading
-        textAlign={["center", "left"]}
-        maxW="200px"
-        size={"sm"}
-        fontFamily={"sans-serif"}
+        textAlign={['center', 'left']}
+        maxW='200px'
+        size={'sm'}
+        fontFamily={'sans-serif'}
         noOfLines={3}
         children={title}
       />
@@ -45,39 +45,39 @@ const Course = ({
 
       <HStack>
         <Text
-          fontWeight={"bold"}
-          textTransform="uppercase"
-          children={"Creator"}
+          fontWeight={'bold'}
+          textTransform='uppercase'
+          children={'Creator'}
         />
 
         <Text
-          fontFamily={"body"}
-          textTransform="uppercase"
+          fontFamily={'body'}
+          textTransform='uppercase'
           children={creator}
         />
       </HStack>
 
       <Heading
-        textAlign={"center"}
-        size="xs"
+        textAlign={'center'}
+        size='xs'
         children={`Lectures - ${lectureCount}`}
-        textTransform="uppercase"
+        textTransform='uppercase'
       />
 
       <Heading
-        size="xs"
+        size='xs'
         children={`Views - ${views}`}
-        textTransform="uppercase"
+        textTransform='uppercase'
       />
 
-      <Stack direction={["column", "row"]} alignItems="center">
+      <Stack direction={['column', 'row']} alignItems='center'>
         <Link to={`/course/${id}`}>
-          <Button colorScheme={"yellow"}>Watch Now</Button>
+          <Button colorScheme={'yellow'}>Watch Now</Button>
         </Link>
         <Button
           isLoading={loading}
-          variant={"ghost"}
-          colorScheme={"yellow"}
+          variant={'ghost'}
+          colorScheme={'yellow'}
           onClick={() => addToPlaylistHandler(id)}
         >
           Add to playlist
@@ -88,47 +88,54 @@ const Course = ({
 };
 
 const Courses = () => {
-  const [keyword, setKeyword] = useState("");
-  const [category, setCategory] = useState("");
+  const [keyword, setKeyword] = useState('');
+  const [category, setCategory] = useState('');
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
 
   const addToPlaylistHandler = async (couseId) => {
     await dispatch(addToPlaylist(couseId));
     dispatch(loadUser());
   };
 
+  const handleNavigateToDetailCourse = (idCourse) => {
+    navigate(`/detailcourse/${idCourse}`);
+  };
+
   const categories = [
-    "Web development",
-    "Artificial Intellegence",
-    "Data Structure & Algorithm",
-    "App Development",
-    "Data Science",
-    "Game Development",
+    'Web development',
+    'Artificial Intellegence',
+    'Data Structure & Algorithm',
+    'App Development',
+    'Data Science',
+    'Game Development',
   ];
 
   const { loading, courses, error, message } = useSelector(
     (state) => state.course
   );
   const [page, setPage] = useState(1);
-  const [limit, setLimit] = useState(9);
+  const [limit, setLimit] = useState(6);
 
   useEffect(() => {
     dispatch(getAllCourses(category, keyword));
 
     if (error) {
       toast.error(error);
-      dispatch({ type: "clearError" });
+      dispatch({ type: 'clearError' });
     }
 
     if (message) {
       toast.success(message);
-      dispatch({ type: "clearMessage" });
+      dispatch({ type: 'clearMessage' });
     }
   }, [category, keyword, dispatch, error, message]);
 
   const indexOfLastPost = page * limit;
   const indexOfFirstPost = indexOfLastPost - limit;
   let currentCoures = [];
+
   currentCoures = courses.slice(indexOfFirstPost, indexOfLastPost);
 
   //page
@@ -139,59 +146,61 @@ const Courses = () => {
     setLimit(pageSize);
   };
   return (
-    <Container minH={"95vh"} maxW="container.lg" paddingY={"8"}>
-      <Heading children="All Courses" m={"8"} />
+    <Container minH={'95vh'} maxW='container.lg' paddingY={'8'}>
+      <Heading children='All Courses' m={'8'} />
 
       <Input
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
-        placeholder="Search a course..."
-        type={"text"}
-        focusBorderColor="yellow.500"
+        placeholder='Search a course...'
+        type={'text'}
+        focusBorderColor='yellow.500'
       />
 
       <HStack
-        overflowX={"auto"}
-        paddingY="8"
+        overflowX={'auto'}
+        paddingY='8'
         css={{
-          "&::-webkit-scrollbar": {
-            display: "none",
+          '&::-webkit-scrollbar': {
+            display: 'none',
           },
         }}
       >
         {categories.map((item, index) => (
-          <Button key={index} onClick={() => setCategory(item)} minW={"60"}>
+          <Button key={index} onClick={() => setCategory(item)} minW={'60'}>
             <Text children={item} />
           </Button>
         ))}
       </HStack>
 
       <Stack
-        direction={["column", "row"]}
-        flexWrap="wrap"
-        justifyContent={["flex-start", "space-evenly"]}
-        alignItems={["center", "flex-start"]}
+        direction={['column', 'row']}
+        flexWrap='wrap'
+        justifyContent={['flex-start', 'space-evenly']}
+        alignItems={['center', 'flex-start']}
       >
         {currentCoures.length > 0 ? (
           currentCoures.map((item) => (
-            <Course
-              key={item._id}
-              title={item.title}
-              description={item.description}
-              views={item.views}
-              imageSrc={item.poster.url}
-              id={item._id}
-              creator={item.createdBy}
-              lectureCount={item.numOfVideos}
-              addToPlaylistHandler={addToPlaylistHandler}
-              loading={loading}
-            />
+            <div onClick={() => handleNavigateToDetailCourse(item._id)}>
+              <Course
+                key={item._id}
+                title={item.title}
+                description={item.description}
+                views={item.views}
+                imageSrc={item.poster.url}
+                id={item._id}
+                creator={item.createdBy}
+                lectureCount={item.numOfVideos}
+                addToPlaylistHandler={addToPlaylistHandler}
+                loading={loading}
+              />
+            </div>
           ))
         ) : (
-          <Heading mt="4" children="Courses Not Found" />
+          <Heading mt='4' children='Courses Not Found' />
         )}
       </Stack>
-      <div className="course__pagination">
+      <div className='course__pagination'>
         <Pagination
           total={courses.length}
           defaultPageSize={limit}
